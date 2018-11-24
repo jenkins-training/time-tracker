ad31e78642f544863fbea8eca61e4c4997e62608
Erick Souza <ericknilsen@gmail.com>
2018-11-24 09:06:10 -0300

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -27,13 +27,7 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-    
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
+     
         return entries.get(index);
     }
 }
